71dd51ff3555c80c901f53351258212697efc7f4
ShadeVI <manuel.t.shade@gmail.com>
2022-03-15 13:53:11 +0000

    numerosPrimos

---
+++ numerosPrimos/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Program
+{
+  class NumerosPrimos
+  {
+    static void Main(string[] args)
+    {
+      int numeroMax = 25;
+      int current = 2;
+
+      while (current <= numeroMax)
+      {
+        bool esPrimo = true;
+
+        for (int divisor = 2; divisor < current; divisor++)
+        {
+          if (current % divisor == 0)
+          {
+            esPrimo = false;
+            break;
+          }
+        }
+        if (esPrimo) Console.Write(current + " ");
+
+        current++;
+      }
+    }
+  }
+}
